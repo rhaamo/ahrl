13731498750c54c2d2ece682b99b10762e8fe6aa
Dashie <dashie@sigpipe.me>
2016-08-22 00:20:05 +0200

    Improved tests using nose-parameterized

--- tests/test_libjambon.py
+++ tests/test_libjambon.py
@@ -3,6 +3,7 @@ sys.path.insert(0, sys.path[0] + '/../')
 from geohelper import bearing
 from libjambon import coordinates2adif, adif2coordinates, adif, geo_bearing_star
 import unittest
+from nose_parameterized import parameterized
 
 
 class TestLibjambon(unittest.TestCase):
@@ -13,58 +14,59 @@ class TestLibjambon(unittest.TestCase):
         pass
 
     # Latitude
-    def test_latitude_pos(self):
-        t = [46.3048, 44.5639, 41.9, 56]
-        for tt in t:
-            a = coordinates2adif(tt, "Latitude")
-            b = adif2coordinates(a)
-            self.assertAlmostEqual(tt, b)
+    @parameterized.expand([(46.3048,), (44.5639,), (41.9,), (56,)])
+    def test_latitude_pos(self, pos):
+        a = coordinates2adif(pos, "Latitude")
+        b = adif2coordinates(a)
+        self.assertAlmostEqual(pos, b)
 
-    def test_latitude_neg(self):
-        t = [-26.9861]
-        for tt in t:
-            a = coordinates2adif(tt, "Latitude")
-            b = adif2coordinates(a)
-            self.assertAlmostEqual(tt, b)
+    @parameterized.expand([(-26.9861,)])
+    def test_latitude_neg(self, pos):
+        a = coordinates2adif(pos, "Latitude")
+        b = adif2coordinates(a)
+        self.assertAlmostEqual(pos, b)
 
     # Longitude
-    def test_longitude_pos(self):
-        t = [10.9409, 12.5]
-        for tt in t:
-            a = coordinates2adif(tt, "Longitude")
-            b = adif2coordinates(a)
-            self.assertAlmostEqual(tt, b)
+    @parameterized.expand([(10.9409,), (12.5,)])
+    def test_longitude_pos(self, pos):
+        a = coordinates2adif(pos, "Longitude")
+        b = adif2coordinates(a)
+        self.assertAlmostEqual(pos, b)
 
-    def test_longitude_neg(self):
-        t = [-3.18, -9.01677, -0.12, -8.73333, -8.04333]
-        for tt in t:
-            a = coordinates2adif(tt, "Longitude")
-            b = adif2coordinates(a)
-            self.assertAlmostEqual(tt, b, places=5)
+    @parameterized.expand([(-3.18,), (-9.01677,), (-0.12,), (-8.73333,), (-8.04333,)])
+    def test_longitude_neg(self, pos):
+        a = coordinates2adif(pos, "Longitude")
+        b = adif2coordinates(a)
+        self.assertAlmostEqual(pos, b, places=5)
 
-    def test_adif(self):
-        t = [['notes', 5], ['call', 4], ['foo bar baz', 11]]
-        for tt in t:
-            a = adif("foo", tt[0])
-            self.assertEqual(a, "<FOO:{0}>{1} ".format(tt[1], tt[0]))
+    @parameterized.expand([
+        ('notes', 5),
+        ('call', 4),
+        ('foo bar baz', 11)
+    ])
+    def test_adif(self, value, size):
+        a = adif("foo", value)
+        self.assertEqual(a, "<FOO:{0}>{1} ".format(size, value))
 
-    def test_bearing(self):
+    @parameterized.expand([
+        (33.54166666666666, 44.64583333333334, 89.5777047195586),
+        (-0.5416666666666572, 38.39583333333334, 191.6123173992922),
+        (18.125, 42.64583333333334, 113.8162224705103)
+    ])
+    def test_bearing(self, lon, lat, bear):
         # Always lon, lat
         _from = [2.208333333333343, 48.97916666666666]
-        to = [[33.54166666666666, 44.64583333333334, 89.5777047195586],
-              [-0.5416666666666572, 38.39583333333334, 191.6123173992922],
-              [18.125, 42.64583333333334, 113.8162224705103]]
-        for tt in to:
-            z = bearing.initial_compass_bearing(_from[1], _from[0], tt[1], tt[0])
-            self.assertEqual(z, tt[2])
+        z = bearing.initial_compass_bearing(_from[1], _from[0], lat, lon)
+        self.assertEqual(z, bear)
 
-    def test_bearing_star(self):
+    @parameterized.expand([
+        (33.54166666666666, 44.64583333333334, "E"),
+        (-0.5416666666666572, 38.39583333333334, "SW"),
+        (18.125, 42.64583333333334, "SE")
+    ])
+    def test_bearing_star(self, lon, lat, bear):
         # Always lon, lat
         _from = [2.208333333333343, 48.97916666666666]
-        to = [[33.54166666666666, 44.64583333333334, "E"],
-              [-0.5416666666666572, 38.39583333333334, "SW"],
-              [18.125, 42.64583333333334, "SE"]]
-        for tt in to:
-            z = bearing.initial_compass_bearing(_from[1], _from[0], tt[1], tt[0])
-            a = geo_bearing_star(z)
-            self.assertEqual(a, tt[2])
+        z = bearing.initial_compass_bearing(_from[1], _from[0], lat, lon)
+        a = geo_bearing_star(z)
+        self.assertEqual(a, bear)
