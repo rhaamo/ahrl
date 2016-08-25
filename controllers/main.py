8a8287e0445a16bc195f9a66cc805bfe4d96c4d1
Dashie <dashie@sigpipe.me>
2016-08-25 22:34:47 +0200

    Add sanity check

--- controllers/main.py
+++ controllers/main.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, abort
 from flask_security import current_user
 from sqlalchemy import func
 
-from models import db, User, Logbook, Log
+from models import db, User, Logbook, Log, DxccEntities, DxccExceptions, DxccPrefixes
 
 bp_main = Blueprint('bp_main', __name__)
 
@@ -10,6 +10,17 @@ bp_main = Blueprint('bp_main', __name__)
 # Show public logbooks
 @bp_main.route('/')
 def home():
+    # Sanity check
+    _dp = db.session.query(DxccPrefixes.id).count()
+    _dx = db.session.query(DxccExceptions.id).count()
+    _de = db.session.query(DxccEntities.id).count()
+    # Count as of 25/08/16 was:
+    # 3665     16435     401
+    # check are lowered to avoid changing them maybe too frequently
+    if _dp < 3500 or _dx < 16300 or _de < 300:
+        flash("DXCC Tables are empty, check manual", "error")
+        abort(500)
+
     pcfg = {"title": "AHRL - Another Ham Radio Log"}
     users = User.query.all()
 
